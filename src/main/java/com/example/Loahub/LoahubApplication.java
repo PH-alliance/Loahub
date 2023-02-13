6bfe812830f342f949d879cf4e2c53198ddc02e9
Gihwan_Park <59417608+GihwanPark@users.noreply.github.com>
2023-02-14 00:00:36 +0900

    pgh 브렌치 추가

---
+++ src/main/java/com/example/Loahub/LoahubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Loahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoahubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Loahub/LoahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
