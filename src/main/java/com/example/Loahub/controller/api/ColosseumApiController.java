02abebaca6e3f2d8912327f7fa91160cf028d519
Lacheln1 <psionict@naver.com>
2023-03-03 14:28:15 +0900

    css적용안되는 문제 해결
    css경로 및  이미지 소스를 타임리프로 묶어주면 해결

--- src/main/java/com/example/Loahub/controller/api/ColosseumApiController.java
+++ src/main/java/com/example/Loahub/controller/api/ColosseumApiController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -31,12 +32,13 @@ public class ColosseumApiController {
     @GetMapping("/pvp")
     public String searchForm(HttpServletRequest httpServletRequest, Model model) throws IOException  {   
         String nickname = httpServletRequest.getParameter("nickname");
-        String testURL = "https://developer-lostark.game.onstove.com/armories/characters/";
+        String reqURL = "https://developer-lostark.game.onstove.com/armories/characters/";
+        // 레헬 토큰 사용중
         String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAxMTI3MTUifQ.Oe1g04-1Vd3b5AGIGd4aVJuzbZr2wargshnTGhKqilDjkQ-M2OWIQO0avaCkPmuGwG5WrMuWK9Af5m5o-qNKe4shQccXMpOolMEWkTnme6so-r6I2u5G64OySCzBO7Tfe5ovpDmA0ZBgcZbTev8rWN7FdFNIgfHsU3g2Fk8r8hGIdxPMYJeFv1wFXHtvhpL7kmeTeWK3HX3M7sJZgNitJMX9gVSdavakjCdpV4o_7Rho6bPwcSmJ_Q0n4VQgUkWyvuEgjfFkAqp7JswZEXQxtspSfRZj0ST-gMR3dzlNno2JgQ-cpD7BA3oiROEgrNUP7t1DEMK6IKfG59f5khuvVg";
         String incodeName = URLEncoder.encode(nickname, "UTF-8");
         
         
-        URL url = new URL(testURL + incodeName + "/colosseums");
+        URL url = new URL(reqURL + incodeName + "/colosseums");
         HttpURLConnection conn=(HttpURLConnection)url.openConnection();
         conn.setRequestMethod("GET");
         conn.setDoOutput(true);
